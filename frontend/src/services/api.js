61675d2ba7dd13aa65558f25e10d18bfdef4e993
Deepanshu112 <140751260+Deepanshu112@users.noreply.github.com>
2025-02-20 19:54:56 +0530

    Add files via upload

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "http://localhost:5000/api",
+  baseURL: process.env.BACKEND_URL,
 });
 
 export const getFlashcards = () => API.get("/flashcards");
